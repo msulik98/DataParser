5f72a68f59ece43d26c77d9302ff937080b3542d
msulik98 <michal.sulik98@gmail.com>
2021-03-08 15:00:37 +0100

    refactored Handler

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,30 +9,22 @@ using Newtonsoft.Json;
 namespace DataParser
 {
 
-    abstract class Handler
+    public class Handler
     {
         protected Handler successor = null;
-        protected IDataParser parser;
+        private IDataParser parser;
+        public Handler(IDataParser parser) => this.parser = parser;
         public void SetSuccessor(Handler successor)
         {
             this.successor = successor;
         }
 
-        public abstract ICollection<User> HandleRequest(string path);
-    }
-    class JsonHandler : Handler
-
-    {
-        public JsonHandler()
-        {
-            this.parser = new JsonParser();
-        }
-        public override ICollection<User> HandleRequest(string path)
+        public ICollection<User> HandleRequest(string path)
         {
             string extension = path.Split('.').Last();
-            if (extension.ToLower()=="json")
+            if (parser.CanIParse(extension))
             {
-               return parser.ParseFile(path);
+                return parser.ParseFile(path);
             }
             else if (successor != null)
             {
@@ -41,56 +33,10 @@ namespace DataParser
             return null;
         }
     }
-    class XMLHandler : Handler
-
-    {
-        public XMLHandler()
-        {
-            this.parser = new XMLParser();
-        }
-        public override ICollection<User> HandleRequest(string path)
-        {
-            string extension = path.Split('.').Last();
-            if (extension.ToLower() == "xml")
-            {
-               return parser.ParseFile(path);
-            }
-            else if (successor != null)
-            {
-                successor.HandleRequest(path);
-            }
-            return null;
-        }
-    }
-
-    class CSVHandler : Handler
-
-    {
-        public CSVHandler()
-        {
-            this.parser = new CSVParser();
-        }
-        public override ICollection<User> HandleRequest(string path)
-        {
-            string extension = path.Split('.').Last();
-            if (extension.ToLower() == "csv")
-            {
-               return parser.ParseFile(path);
-            }
-            else if (successor != null)
-            {
-                successor.HandleRequest(path);
-            }
-            return null;
-        }
-    }
-
-
-
-
     public interface IDataParser
     {
         ICollection<User> ParseFile(string path);
+        bool CanIParse(string extension);
     }
     public interface IDisplayable
     {
@@ -102,9 +48,9 @@ namespace DataParser
         static void Main(string[] args)
         {
             string[] files = new string[] {"file.json","file.xml","file.csv"};
-            Handler json = new JsonHandler();
-            Handler xml = new XMLHandler();
-            Handler csv = new CSVHandler();
+            Handler json = new Handler(new JsonParser());
+            Handler xml = new Handler(new XMLParser());
+            Handler csv = new Handler(new CSVParser());
             json.SetSuccessor(xml);
             xml.SetSuccessor(csv);
             foreach (var file in files)
@@ -127,6 +73,8 @@ namespace DataParser
         public JsonParser()
         {
         }
+        public bool CanIParse(string extension) => extension == "json";
+
         public ICollection<User> ParseFile(string path)
         {
             string jsonString = File.ReadAllText(path);
@@ -139,6 +87,7 @@ namespace DataParser
         public XMLParser()
         {
         }
+        public bool CanIParse(string extension) => extension == "xml";
         public ICollection<User> ParseFile(string path)
         {
             Console.WriteLine("XML has been parsed! :)))");
@@ -150,6 +99,7 @@ namespace DataParser
         public CSVParser()
         {
         }
+        public bool CanIParse(string extension) => extension == "csv";
         public ICollection<User> ParseFile(string path)
         {
             Console.WriteLine("CSV has been parsed! :)))");
